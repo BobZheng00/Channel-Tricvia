c8d0ae2150b31f8ea2f45c26e3d95a9e9ab9d1f4
unknown <shenyanzheng00@gmail.com>
2022-08-09 17:12:37 -0400

    initial migration

--- Channel_Trivia/settings.py
+++ Channel_Trivia/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'game'
+    'game.apps.GameConfig'
 ]
 
 MIDDLEWARE = [
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'Channel_Trivia.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Channel Trivia',
+        'USER': 'postgres',
+        'PASSWORD': '720521',
+        'HOST': 'localhost'
     }
 }
 
