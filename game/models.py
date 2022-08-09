914e2054a4225059f2eff4bc57fd8a3c74ef0f7b
unknown <shenyanzheng00@gmail.com>
2022-08-09 16:58:52 -0400

    reconnect

--- game/models.py
+++ game/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+
+# class QuestionsSet(models.Model):
+#     type = models.CharField(max_length=100)
+#     category = models.CharField(max_length=100)
+#     question = models.TextField()
+#     correct_answer = models.CharField(max_length=100)
+#     incorrect_answer = postgres.ArrayField(models.CharField(max_length=100, null=True, blank=True), null=True,
+#                                            blank=True)
+#
+#     def load_data(self):
+#         with open('C:/Users/23116/Trivial-Questions/questions_data.json') as json_file:
+#             data = json.load(json_file)
+#         for question in data['questions']:
+#             self.objects.create(type='question', category=str(question['category']), question=str(question['question']),
+#                                 correct_answer=str(question["correct answer"]), incorrect_answer=None)
